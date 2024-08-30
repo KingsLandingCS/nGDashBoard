7ba452af593369b92247873d6add959453956854
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-31 02:32:38 +0500

    learning

---
+++ src/app/admin-portal/admin-components/virtual-reality/virtual-reality.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual-reality',
+  standalone: true,
+  imports: [],
+  templateUrl: './virtual-reality.component.html',
+  styleUrl: './virtual-reality.component.scss'
+})
+export class VirtualRealityComponent {
+
+}
