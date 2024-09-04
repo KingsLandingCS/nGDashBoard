5ed93f537aa86501679c829952a6fc70161d863f
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-09-04 08:02:26 +0500

    learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from "./admin-portal/admin-components/dashboard/dashboard.component";
+import { SidebarComponent } from "./admin-portal/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './admin-portal/admin-components/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', }
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  {path: 'sidebar', component: DashboardComponent}
 ];
