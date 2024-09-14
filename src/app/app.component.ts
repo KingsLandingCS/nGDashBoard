0e18c70d83d5d7814c40740c007887282efef47c
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-09-14 10:58:08 +0500

    learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from "./admin-portal/admin-components/dashboard/dashboard.component";
 import { SidebarComponent } from "./admin-portal/sidebar/sidebar.component";
+import { HeaderComponent } from "./header/header.component";
+import { BillingComponent } from "./admin-portal/admin-components/billing/billing.component";
+import { NotificationComponent } from "./admin-portal/admin-components/notification/notification.component";
+import { ProfileComponent } from "./admin-portal/admin-components/profile/profile.component";
+import { TableComponent } from "./admin-portal/admin-components/table/table.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent, SidebarComponent],
+  imports: [RouterOutlet, DashboardComponent, SidebarComponent, HeaderComponent, BillingComponent, NotificationComponent, ProfileComponent, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
+import { TableComponent } from './admin-portal/admin-components/table/table.component';
+import { BillingComponent } from './admin-portal/admin-components/billing/billing.component';
+import { VirtualRealityComponent } from './admin-portal/admin-components/virtual-reality/virtual-reality.component';
+import { NotificationComponent } from './admin-portal/admin-components/notification/notification.component';
+import { ProfileComponent } from './admin-portal/admin-components/profile/profile.component';
 import { DashboardComponent } from './admin-portal/admin-components/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  {path: 'sidebar', component: DashboardComponent}
+  { path: 'table', component: TableComponent },
+  { path: 'billing', component: BillingComponent },
+  { path: 'virtual-reality', component: VirtualRealityComponent },
+  { path: 'notification', component: NotificationComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
